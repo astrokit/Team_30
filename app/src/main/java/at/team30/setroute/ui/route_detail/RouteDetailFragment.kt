38a7f615ed4fb47e54d7b864e5da283ec19325cb
Dmytro Shvets <79966575+Deerax@users.noreply.github.com>
2021-06-02 16:27:32 +0200

    SR-039 Zoom-in map picture [SD,PO]

--- app/src/main/java/at/team30/setroute/ui/route_detail/RouteDetailFragment.kt
+++ app/src/main/java/at/team30/setroute/ui/route_detail/RouteDetailFragment.kt
@@ -5,23 +5,22 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.view.isVisible
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
-import androidx.transition.Visibility
 import at.team30.setroute.Helper.NavigationHelper
 import at.team30.setroute.R
 import at.team30.setroute.ui.settings.SettingsFragment
 import com.google.android.material.button.MaterialButtonToggleGroup
+import com.stfalcon.imageviewer.StfalconImageViewer
 import com.zeugmasolutions.localehelper.LocaleHelper
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -40,6 +39,10 @@ class RouteDetailFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_route_detail, container, false)
+    }
+
+    fun loadImage(view : ImageView, image : Bitmap) {
+        view.setImageBitmap(image)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -85,6 +88,13 @@ class RouteDetailFragment : Fragment() {
             }
         }
         viewModel.getImageLiveData().observe(viewLifecycleOwner,imageObserver);
+
+        imageView.setOnClickListener {
+            StfalconImageViewer.Builder(context, listOf(imageView.drawable.toBitmap()), ::loadImage)
+                .withHiddenStatusBar(false)
+                .withTransitionFrom(imageView)
+                .show()
+        }
 
         val walkedButton = view.findViewById<MaterialButtonToggleGroup>(R.id.walked_button_group);
 
