5494d6562b3373544c1eb5a64aeba49e0f7dd1c0
Olha Pavlenko <olha.pavlenko@student.tugraz.at>
2021-06-09 15:02:32 +0200

    SR-042 Added Filtering Test [EJ,PO]

---
+++ app/src/test/java/at/team30/setroute/FilteringTest.kt
@@ -0,0 +1,48 @@
+package at.team30.setroute
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import at.team30.setroute.infrastructure.*
+import at.team30.setroute.models.FilteringOptions
+import at.team30.setroute.models.SortingOptions
+import at.team30.setroute.ui.routes.RouteListViewModel
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class FilteringTest {
+    @Rule
+    @JvmField
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val mockRepository: IRoutesRepository = mockk()
+    private val settingRepository : ISettingRepository = SettingRepository()
+    private val filteringRepository : IFilterRepository = FilteringRepository()
+
+    @Before
+    fun init() {
+        every { mockRepository.getRoutes() } returns Fixtures.routes_filtering()
+        for (route in Fixtures.routes_filtering()) {
+            every { mockRepository.getRoutesById(route.id) } returns route
+        }
+    }
+
+    @Test
+    fun `filter by Coffee Lovers`(){
+        // Arrange
+        val sut = RouteListViewModel(mockRepository, settingRepository, filteringRepository)
+        filteringRepository.storeFilteringOptions(FilteringOptions(listOf(3)))
+
+        // Act
+        val result = sut.getRoutes().value
+
+        // Assert
+        assertEquals(1, result?.size)
+    }
+}
