ec501cd0495ae32e6c4be630a903924adfbe7810
Bohdan Lailo <bogdanlailo@gmail.com>
2021-06-09 15:31:33 +0200

    SR-033 Test that email received [LB,MH]

--- app/src/test/java/at/team30/setroute/EmailTest.kt
+++ app/src/test/java/at/team30/setroute/EmailTest.kt
@@ -1,6 +1,10 @@
 package at.team30.setroute
 
 import at.team30.setroute.Helper.EmailHelper
+import com.dumbster.smtp.SimpleSmtpServer
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -8,6 +12,10 @@ import java.lang.IllegalArgumentException
 
 @RunWith(JUnit4::class)
 class EmailTest {
+
+    lateinit var emailHelper : EmailHelper
+    lateinit var smtpServer : SimpleSmtpServer
+    var port = 8080
 
     @Test(expected = IllegalArgumentException::class)
     fun initIllegalArgumentsNoUsername() {
@@ -20,18 +28,49 @@ class EmailTest {
     @Test(expected = IllegalArgumentException::class)
     fun initIllegalArgumentsNoPassword() {
         EmailHelper(
-                authenticate = true,
-                userName = "Max Musterman",
-                password = null
+            authenticate = true,
+            userName = "Max Musterman",
+            password = null
         )
     }
+
+
 
     @Test
     fun createEmailHelperWithoutAuthentication() {
         EmailHelper(
-                authenticate = false,
-                userName = "Max Musterman",
-                password = null
+            authenticate = false,
+            userName = "Max Musterman",
+            password = null
         )
     }
+
+    @Before
+    fun setUpTest() {
+        smtpServer = SimpleSmtpServer(port)
+    }
+
+    @Test
+    fun emailReceived() {
+        var text = "Very best feedback."
+        var receiver = "some@anything.org"
+        var subject = "Very important issue"
+
+        emailHelper = EmailHelper(
+            smtpHost = "localhost",
+            smtpPort = port,
+            enableStartTtls = false,
+            userName = null,
+            password = null,
+            sender = "bestemail@provider.com",
+            authenticate = false
+        )
+
+        runBlocking {
+            emailHelper.sendEmail(text, receiver, subject)
+        }
+        Assert.assertEquals(1, smtpServer.receivedEmailSize)
+    }
+
+
 }
